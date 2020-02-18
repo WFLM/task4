e8eff0371bb9c5c1ddcbdd0e5cd6456e7e3586b8
Yury Kuznetsov <yurykuznetsov@protonmail.com>
2020-02-19 00:24:40 +0300

    add rooms_top5_min_average_age in db_get_data

--- task4/db_get_data.py
+++ task4/db_get_data.py
@@ -4,7 +4,6 @@ class DBGetData:
 
     def _rooms_number_of_students(self):
         cursor = self._db_connection.cursor()
-
         sql_query = """
         SELECT r.room_id, r.room_name, COUNT(s.room_id) as num_of_students
         FROM rooms_accounting.Students as s, rooms_accounting.Rooms as r
@@ -15,3 +14,25 @@ class DBGetData:
         data = cursor.fetchall()  # (id, room_name, num_of_students)
         cursor.close()
         return data
+
+    def _rooms_top5_min_average_age(self):
+        cursor = self._db_connection.cursor()
+        sql_query = """
+        SELECT r.room_id, r.room_name,
+               AVG(
+                    (
+                      (YEAR(CURRENT_DATE) - YEAR(s.birthday)) -
+                      (DATE_FORMAT(CURRENT_DATE, '%m%d') < DATE_FORMAT(s.birthday, '%m%d'))
+                    )
+                  ) AS avg_age
+        FROM Students as s, Rooms as r
+        WHERE s.room_id = r.room_id
+        GROUP BY s.room_id
+        ORDER BY avg_age
+        LIMIT 5;
+        """
+        cursor.execute(sql_query)
+        data = cursor.fetchall()  # (id, room_name, average_age)
+        cursor.close()
+        return data
+
