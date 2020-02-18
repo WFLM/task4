91eb03e0ada0d820c85d6e8f41d0ef92887d9b0d
Yury Kuznetsov <yurykuznetsov@protonmail.com>
2020-02-18 21:57:57 +0300

    add db_fill

---
+++ task4/db_fill.py
@@ -0,0 +1,21 @@
+class DBFill:
+    def __init__(self, db_connection, rooms_objects, students_objects):
+        self._connection = db_connection
+        self._rooms_objects = rooms_objects
+        self._students_objects = students_objects
+
+    def _fill_rooms(self):
+        cursor = self._connection.cursor()
+        sql_query = "INSERT INTO Rooms(room_id, room_name) VALUES (%s, %s)"
+        cursor.executemany(sql_query, self._rooms_objects)
+        cursor.close()
+
+    def _fill_students(self):
+        cursor = self._connection.cursor()
+        sql_query = "INSERT INTO Students(student_id, student_name, sex, birthday, room_id) VALUES (%s, %s, %s, %s, %s)"
+        cursor.executemany(sql_query, self._students_objects)
+        cursor.close()
+
+    def fill_db(self):
+        self._fill_rooms()
+        self._fill_students()
